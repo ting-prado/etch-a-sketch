c53a3668a777fb899d57ae56102a836fbf31daff
Ethel <ethel.prado1998@gmail.com>
2021-07-23 13:52:54 +0800

    Put style on UI

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,7 @@ const container = document.querySelector('#container');
 function createGrid(size){
     for(let i=1; i<=(size*size); i++){
         const div = document.createElement('div');
-        div.setAttribute('style', `width: ${700/size}px; height: ${600/size}px`);
+        div.setAttribute('style', `width: ${700/size}px; height: ${550/size}px`);
         div.classList.add('div-style');
         container.appendChild(div);
     }
