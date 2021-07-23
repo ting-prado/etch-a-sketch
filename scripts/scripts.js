6fc73eec25b19f86f1517197223488d91f9441e1
Ethel <ethel.prado1998@gmail.com>
2021-07-23 14:30:55 +0800

    Added function that would return random colors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,15 +8,23 @@ function createGrid(size){
         const div = document.createElement('div');
         div.setAttribute('style', `width: ${700/size}px; height: ${550/size}px`);
         div.classList.add('div-style');
+        div.addEventListener('mouseover', makeRainbow);
         container.appendChild(div);
     }
+}
+
+function makeRainbow(e) {
+    const hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    let randomColor = '#';
+    for(let i=0; i<6; i++){
+        randomColor += hex[Math.floor(Math.random()*(hex.length-1))];
+    }
+    this.style.backgroundColor = `${randomColor}`;
 }
 
 function makeBlack(e) {
     this.classList.add('makeBlack');
 }
-
-console.dir(document.querySelector('#color-picker').value);
 
 createGrid(16);
 
